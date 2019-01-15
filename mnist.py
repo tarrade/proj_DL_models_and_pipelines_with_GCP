7fd23b803d710928758bef185230b272774e44f8
henry-webel <henry.webel@axa-winterthur.ch>
2019-01-15 17:27:07 +0100

    eager vs lazy tf.model

--- mnist.py
+++ mnist.py
@@ -6,17 +6,11 @@ Basic ideas frm https://github.com/aymericdamien/TensorFlow-Examples/blob/master
 import tensorflow as tf
 import numpy as np
 
-
 NUM_EPOCHS = 12
 BATCH_SIZE = 64
-LEARNING_RATE = 0.1
-
-
+LEARNING_RATE = 0.001
 ###############################################################################
-# # Decide if you want eager execution:
-# EAGER = True
-# if EAGER:
-#     tf.enable_eage_execution() #  ['config=None', 'device_policy=None', 'execution_mode=None']
+#Load Data
 try:
     raise Exception
     from tensorflow.keras.datasets import mnist
@@ -36,18 +30,15 @@ x_train = x_train.reshape(len(x_train), -1)
 x_test = x_test.reshape(len(x_test), -1)
 
 # Convert Numpy Array to Tensor manually to avoid accidential reassignment:
-# x_train = tf.convert_to_tensor(x_train, name = None)
-# x_test  = tf.convert_to_tensor(x_test, name = None)
-
+# x_train = tf.cast(x_train, dtype="float")
+# x_test  = tf.cast(x_test, dtype="float")
 print("passed")
-
 
 def oneHotEncode(array):
     n = len(array)
     dense_array = np.zeros((n, len(set(array))))
     dense_array[np.arange(n), array] = 1
     return dense_array
-
 
 assert set(y_train) == set(
     y_test), "Classes in train and test set are different. which is correct?"
@@ -59,8 +50,6 @@ y_test = oneHotEncode(y_test)
 
 # Use `tf.parse_single_example()` to extract data from a `tf.Example`
 # protocol buffer, and perform any additional per-record preprocessing.
-
-
 def parser(record):
     """
     Define a function to pass to map fct of tf.data.Dataset
@@ -88,73 +77,79 @@ def parser(record):
     return {"image_data": image, "date_time": parsed["date_time"]}, label
 ###############################################################################
 # Build Model
-
-
 # data specific parameters:
 num_classes = y_train.shape[-1]  # MNIST total classes (0-9 digits)
-num_input = x_train.shape[-1]  # MNIST data input (img shape: 28*28)
-
+dim_input = x_train.shape[-1]  # MNIST data input (img shape: 28*28)
 
 # Number of units in hidden layer (Hyperparameter)
 n_hidden_1 = 256  # 1st layer number of neurons
 n_hidden_2 = 256  # 2nd layer number of neurons
 
-
 # tf Graph input
-X = tf.placeholder("float", shape=[None, num_input])
+X = tf.placeholder("float", shape=[None, dim_input])
 Y = tf.placeholder("int32", shape=[None, num_classes])
 
-# Store layers weight & bias
-weights = {
-    'h1': tf.Variable(tf.random_normal([num_input, n_hidden_1])),
-    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([n_hidden_2, num_classes]))
-}
-biases = {
-    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([num_classes]))
-}
-
-# Create model
 
 
-def neural_net(x):
-    # Hidden fully connected layer with 256 neurons
-    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    # Hidden fully connected layer with 256 neurons
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    # Output fully connected layer with a neuron for each class
-    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
-    return out_layer
+# # Create model a statful model
+class FNN(object):
+  def __init__(self):  
+    self.w_1 = tf.Variable(tf.Variable(tf.random_normal([dim_input, n_hidden_1])), name='W1')
+    self.b_1 = tf.Variable(tf.random_normal([n_hidden_1]), name='b1')
+    self.w_2 = tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]), name="W2")
+    self.b_2 = tf.Variable(tf.random_normal([n_hidden_2]), name='b2')
+    self.w_out = tf.Variable(tf.random_normal([n_hidden_2, num_classes]), name="W_out")
+    self.b_out = tf.Variable(tf.random_normal([num_classes]), name='b_out')
+    #self.weights = [self.w_1, self.b_1, self.w_2, self.b_2, self.w_out, self.b_out]
+  
+  def __call__(self, inputs, training=False):
+    hidden_1 = tf.nn.relu(tf.matmul(inputs, self.w_1) + self.b_1)
+    hidden_2 = tf.nn.relu(tf.matmul(hidden_1, self.w_2) + self.b_2)
+    logits =   tf.matmul(hidden_2, self.w_out) + self.b_out
+    return logits
 
-# # Using tf.layers:
-# tf.layers.dense(X, activation= tf... relu, units= n_hidden_1)
+# class FNN(tf.keras.Model):
+#   def __init__(self):
+#     super(FNN, self).__init__()
+#     self.w_1 = tf.Variable(tf.Variable(tf.random_normal([dim_input, n_hidden_1])), name='W1')
+#     self.b_1 = tf.Variable(tf.random_normal([n_hidden_1]), name='b1')
+#     self.w_2 = tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]), name="W2")
+#     self.b_2 = tf.Variable(tf.random_normal([n_hidden_2]), name='b2')
+#     self.w_out = tf.Variable(tf.random_normal([n_hidden_2, num_classes]), name="W_out")
+#     self.b_out = tf.Variable(tf.random_normal([num_classes]), name='b_out')
+#     #self.weights = [self.w_1, self.b_1, self.w_2, self.b_2, self.w_out, self.b_out]
+  
+#   def call(self, inputs, training=False):
+#     hidden_1 = tf.nn.relu(tf.matmul(inputs, self.w_1) + self.b_1)
+#     hidden_2 = tf.nn.relu(tf.matmul(hidden_1, self.w_2) + self.b_2)
+#     logits =   tf.matmul(hidden_2, self.w_out) + self.b_out
+#     return logits
+
+# Construct model
+
+logits = FNN()(X)
 
 
 # Construct model
-logits = neural_net(X)
-prediction = tf.nn.softmax(logits)
+
+logits = FNN()(X)
 
 # Define loss and optimizer
-loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
-    logits=logits, labels=Y))
-optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
-train_op = optimizer.minimize(loss_op)
+# loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
+#     logits=logits, labels=Y))
+
+loss = tf.losses.softmax_cross_entropy(logits=logits, onehot_labels=Y)
+optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(loss)
+# train_op = optimizer.minimize(loss_op)
 
 # Evaluate model
-correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+tp = tf.equal(tf.argmax(logits, axis=1), tf.argmax(Y, axis=1))
+accuracy = tf.reduce_mean(tf.cast(tp, dtype="float"))
 
 # Initialize the variables (i.e. assign their default value)
 # init = tf.global_variables_initializer()  # ToDo: depreciated
-
-
 ##############################################################################
-#
 # tf.data , Preprocessing
-
-# Load from Numpy, strategy: https://www.tensorflow.org/guide/datasets#consuming_numpy_arrays
 trainslices = tf.data.Dataset.from_tensor_slices((X, Y))
 trainslices = trainslices.shuffle(buffer_size=3000,
                                   seed=123456,
@@ -178,6 +173,12 @@ testslices = tf.data.Dataset.from_tensor_slices((X, Y))
 
 # Sequencing batch to mini-batches
 # https://github.com/tensorflow/tensorflow/blob/9230423668770036179a72414482d45ddde40a3b/tensorflow/contrib/training/python/training/sequence_queueing_state_saver.py#L353
+
+
+N_train = x_train.shape[0]
+n_batches = N_train / BATCH_SIZE
+step = int(n_batches/ 60)
+
 init = tf.global_variables_initializer()
 # Start training
 with tf.Session() as sess:
@@ -185,33 +186,36 @@ with tf.Session() as sess:
     # sess.run(init)
     sess.run(init)
     print('Initalized graph')
-    for _ in range(NUM_EPOCHS):
-        # i = 0
-        # print("#### Epoch {}".format(_))
+    for i in range(1, NUM_EPOCHS+1):
+        print("Epoch {}: ".format(i), end='')
         sess.run(iterator.initializer, feed_dict={X: x_train,
                                                   Y: y_train})
+        batch = 0
         while True:
             try:
-                # i += 1
-                # if i % 100 == 0:
-                #     print("# batch {}".format(i))
-                batch_x, batch_y = sess.run(next_element)
-                assert batch_x.shape == (
-                    BATCH_SIZE, num_input), "Something is wrong with Batch shape: {}".format(batch_x.shape)
+                images, labels = sess.run(next_element)
+                assert images.shape == (
+                    BATCH_SIZE, dim_input), "Something is wrong with Batch shape: {}".format(images.shape)
                 # Run optimization op (backprop)
-                sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
+                sess.run(optimizer, feed_dict={X: images, Y: labels})
+                if batch % step == 0:
+                    print('#', end='')
+                batch += 1
             except tf.errors.OutOfRangeError:
                 # Calculate metrics for validation set / test set
-                loss, acc = sess.run([loss_op, accuracy], feed_dict={X: x_train,
-                                                                     Y: y_train})
-                print("Epoch " + str(_ + 1) + ", Training Loss= " +
-                      "{:.4f}".format(loss) + ", Training Accuracy= " +
-                      "{:.3f}".format(acc))
+                _loss, _acc = sess.run([loss, accuracy], feed_dict={X: x_train,
+                                                                       Y: y_train})
+                print(", Training Loss= {:.2f}".format(_loss) +
+                      ", Training Accuracy= {:.3f}".format(_acc))
                 break
-
-    print("Optimization Finished!")
-
-    # Calculate accuracy for MNIST test images
-    print("Testing Accuracy:",
+        print("Validation Accuracy:",
           sess.run(accuracy, feed_dict={X: x_test,
                                         Y: y_test}))
+
+
+    # print("Optimization Finished!")
+
+    # # Calculate accuracy for MNIST test images
+    # print("Validation Accuracy:",
+    #       sess.run(accuracy, feed_dict={X: x_test,
+    #                                     Y: y_test}))
