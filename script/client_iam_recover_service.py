6383a05500c545278fabd753a496d542afee1952
Henry Webel <henry.webel@posteo.de>
2019-04-25 17:27:00 +0200

    update image and sample script

--- script/client_iam_recover_service.py
+++ script/client_iam_recover_service.py
@@ -1,24 +1,28 @@
 import os
 
-from google.oauth2 import service_account
 import googleapiclient.discovery
 
-PROJECT_ID = ''
-SERVICE_ACCOUNT_ID = ''
-
-# # Get credentials
-# credentials = service_account.Credentials.from_service_account_file(
-#     filename=os.environ['GOOGLE_APPLICATION_CREDENTIALS'],
-#     scopes=['https://www.googleapis.com/auth/cloud-platform'])
+#replace with correct information:
+PROJECT_ID = 'ml-productive-pipeline-12345'
+SERVICE_ACCOUNT_UNIQUE_ID = '111111111111111111111'
+SERVICE_ACCOUNT_EMAIL_ID = '123456789123-compute@developer.gserviceaccount.com'
 
 # Create the Cloud IAM service object
-service = googleapiclient.discovery.build(
-    'iam', 'v1')
+service = googleapiclient.discovery.build('iam', 'v1') # credentials from Cloud SDK
 
 # Call the Cloud IAM Roles API
-# If using pylint, disable weak-typing warnings
-# pylint: disable=no-member
-body = "projects/{project_id}/serviceAccounts/{key_id}".format(project_id=PROJECT_ID, key_id=SERVICE_ACCOUNT_ID)
+
+# Did not work for now
+body = "projects/{project_id}/serviceAccounts/{key_id}/".format(
+    project_id=PROJECT_ID, 
+    key_id=SERVICE_ACCOUNT_UNIQUE_ID)
+
+request = service.projects().serviceAccounts().undelete(name=body)
+print(request.execute())
+
+body = "projects/{project_id}/serviceAccounts/{key_id}".format(
+    project_id=PROJECT_ID, 
+    key_id=SERVICE_ACCOUNT_EMAIL_ID)
 
 request = service.projects().serviceAccounts().undelete(name=body)
 print(request.execute())
