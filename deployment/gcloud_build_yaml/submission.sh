36f9169fc2be7a2065711aabf9c83109f58a5724
tarrade <fabien.tarrade@gmail.com>
2019-06-03 12:12:15 +0200

    update and clean up : yaml and jinja

--- deployment/gcloud_build_yaml/submission.sh
+++ deployment/gcloud_build_yaml/submission.sh
@@ -1,5 +1,8 @@
-# deploy a notebok instance
-gcloud deployment-manager deployments create deployment-notebook-v01 --config create_optimized_notebook_instance.yaml
+# deploy an optimized notebok instance
+#gcloud deployment-manager deployments create deployment-notebook-v01 --config create_optimized_notebook_instance.yaml
+
+# deploy an standard notebok instance
+gcloud deployment-manager deployments create deployment-notebook-sandor-v01 --config create_standard_notebook_instance.yaml
 
 # deploy a docker VM
-gcloud deployment-manager deployments create deployment-debian-v01 --config create_docker_vm.yaml
+#gcloud deployment-manager deployments create deployment-debian-v01 --config create_docker_vm.yaml
