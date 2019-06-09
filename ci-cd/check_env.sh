b99296440156646074a97feccb883c33349ac478
tarrade <fabien.tarrade@gmail.com>
2019-06-09 18:25:32 +0200

    adding trigger and check env

---
+++ ci-cd/check_env.sh
@@ -0,0 +1,21 @@
+echo "Checking input env variable:"
+echo "--> BUILD"
+echo $BUILD
+echo "--> PROJECT"
+echo $PROJECT
+echo "--> REV"
+echo $REV
+echo "--> NAME_INSTANCE"
+echo $BUILD
+echo "--> TAG"
+echo $TAG
+echo "--> DOCKERFILE"
+echo $DOCKERFILE
+echo "--> OWNER"
+echo $OWNER
+echo "--> NETWORK"
+echo $NETWORK
+echo "--> SUBNET"
+echo $SUBNET
+echo "--> BUCKET"
+echo $BUCKET
